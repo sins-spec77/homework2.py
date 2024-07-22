03a31f5af097fc7157598a06763fa17faaabfc45
sins-spec77 <timmimiki1@gmail.com>
2024-07-22 13:36:17 +0300

    homework2.py

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework = '12,'
+time = '1.5,'
+course_name = 'python,'
+TimeForOneTask = 1.5 / 12
+print('Курс',course_name,'всего задач:',homework,'затрачено время:', time, 'среднее время выполнения' ,TimeForOneTask) 
