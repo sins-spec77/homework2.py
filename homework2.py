e4d248fb7b6564a6ee07ff0292bf7989da81bad8
sins-spec77 <timmimiki1@gmail.com>
2024-07-22 18:26:20 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 homework = '12,'
 time = '1.5,'
 course_name = 'python,'
-TimeForOneTask = 1.5 / 12
-print('Курс',course_name,'всего задач:',homework,'затрачено время:', time, 'среднее время выполнения' ,TimeForOneTask) 
+time_for_one_task = 1.5 / 12
+print('Курс',course_name,'всего задач:',homework,'затрачено время:', time, 'среднее время выполнения' ,time_for_one_task)
